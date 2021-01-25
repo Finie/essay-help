a735db5c50004bf15bbd5afbc2d957921d118b97
unknown <finiephenius@gmail.com>
2021-01-25 15:23:48 +0300

    web refactoring

--- src/components/pages/main/reviews/reviews.js
+++ src/components/pages/main/reviews/reviews.js
@@ -9,19 +9,20 @@ function Review() {
     <div className="reviews-container">
       <div className="leading">
         <h1>
-          Essay<span style={{ color: " #F4D140" }}>help</span> Latest Service
+          Legal<span style={{ color: " #F4D140" }}>essay</span>writers Service
           Reviews (Customer Feedback){" "}
         </h1>
         <div className="desctiption">
           <section>
-            Customer satisfaction is a top priority here at legalessaywriters.com We do
-            everything in our power to make sure that we stay as the best essay
-            writing service in the business. Apparently, our customers tend to
-            be very satisfied with our reviews and this is evident with the
-            positive feedback that we receive. Every online essay writer has
-            overcome countless challenges while working with us. So, if you need
-            a second opinion on whether or not to order an essay from us, check
-            out what our past clients have to say!
+            Customer satisfaction is a top priority here at
+            legalessaywriters.com We do everything in our power to make sure
+            that we stay as the best essay writing service in the business.
+            Apparently, our customers tend to be very satisfied with our reviews
+            and this is evident with the positive feedback that we receive.
+            Every online essay writer has overcome countless challenges while
+            working with us. So, if you need a second opinion on whether or not
+            to order an essay from us, check out what our past clients have to
+            say!
           </section>
           <section className="note">
             NB: This information is based on reviews submited by clients in the
@@ -31,13 +32,16 @@ function Review() {
       </div>
 
       <div className="reviews">
-         <h1><MdRateReview style={{marginRight:"2rem"}}/>Client Reviews</h1>
-         {reviewData.data.map((item)=><Reviewitem data={item} />)}
-         </div>
+        <h1>
+          <MdRateReview style={{ marginRight: "2rem" }} />
+          Client Reviews
+        </h1>
+        {reviewData.data.map((item) => (
+          <Reviewitem data={item} />
+        ))}
+      </div>
 
-        <Button>Show More</Button>
-
-
+      <Button>Show More</Button>
     </div>
   );
 }
